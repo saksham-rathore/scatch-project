4eb50999db69a562431146a8acb966ad84cd1452
saksham762007 <samrathore07062007@gmail.com>
2026-03-08 10:32:43 +0530

    file update

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 
 const router = express.Router();
-
+router.post("/register", (req, res) => {
+    res.send("register route working")
+})
 
 
 module.exports = router;
