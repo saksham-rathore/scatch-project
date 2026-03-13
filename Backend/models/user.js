7a1af6e43976737f88c1cb98ea3e166b21c973ec
saksham762007 <samrathore07062007@gmail.com>
2026-03-13 19:18:56 +0530

    file update

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -3,9 +3,18 @@ const moongose = require("mongoose");
 moongose.connect("mongodb://127.0.0.1:27017/scatch");
 
 const userschema = moongose.Schema({
-    name: String,
-    email: String,
-    password: String,
+    name:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    }
 });
 
 module.exports = mongoose.model('user', userschema);
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
-const user = require("../models/user");
+const user = require("./models/user");
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+app.get('/create', async (req, res) => {
+    let createduser = await userModel.create({
+        name,
+        email,
+        password,
+    })
+
+    res.send(createduser);
 })
+
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
